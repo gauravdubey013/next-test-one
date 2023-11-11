4b974e5d6cd32b4c06f4b084ec74f054ac55f967
Nilesh Dubey <nileshdubey@Nileshs-MacBook-Pro.local>
2023-11-11 05:58:30 +0530

    nextjs 1st test repo

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    config.infrastructureLogging = { debug: /PackFileCache/ };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
